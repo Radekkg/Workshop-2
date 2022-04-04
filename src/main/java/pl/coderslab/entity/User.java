53371bb8e719fe6d4ba1ceabc85742926aa3e675
radoslaw <radoslaw.lewek@gmail.com>
2022-04-04 22:42:40 +0200

    int id ----> long id and refactoring methods

--- src/main/java/pl/coderslab/entity/User.java
+++ src/main/java/pl/coderslab/entity/User.java
@@ -2,16 +2,16 @@ package pl.coderslab.entity;
 
 public class User {
 
-    private int id;
+    private long id;
     private String userName;
     private String email;
     private String password;
 
-    public int getId(){
+    public long getId(){
         return id;
     }
 
-    public void setId(int id){
+    public void setId(long id){
         this.id = id;
     }
 
