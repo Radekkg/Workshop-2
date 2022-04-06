7daf984a30d571d514e5817e21f9bc1cd0d81d34
radoslaw <radoslaw.lewek@gmail.com>
2022-04-06 21:34:42 +0200

    create new User

--- src/main/java/pl/coderslab/MainDao.java
+++ src/main/java/pl/coderslab/MainDao.java
@@ -3,6 +3,8 @@ package pl.coderslab;
 import pl.coderslab.entity.User;
 import pl.coderslab.entity.UserDao;
 
+import java.util.Arrays;
+
 public class MainDao {
     public static void main(String[] args) {
         User user = new User();
@@ -10,7 +12,13 @@ public class MainDao {
         user.setUserName("Laurencja");
         user.setPassword("haslo123");
 
+        User user1 = new User();
+        user1.setEmail("Dominikos@yahoo.com");
+        user1.setUserName("Dominikos");
+        user1.setPassword("qwerty123");
+
         UserDao userDao = new UserDao();
-        userDao.create(user);
+        //userDao.create(user1);
+        System.out.println(Arrays.toString(userDao.findAll()));
     }
 }
